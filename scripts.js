a2fb20376124c50fe100cee00ed8464638636fc9
xGreylee <13160675187@163.com>
2018-06-26 16:21:19 +0800

    update gitignore

--- scripts.js
+++ scripts.js
@@ -4,27 +4,28 @@ const MongoClient = require('mongodb').MongoClient;
 (async () => {
 	const workbook = new Excel.Workbook()
 	const client = await MongoClient.connect('mongodb://localhost:27017')
-	workbook.xlsx.readFile('./public/commentdata.xlsx').then(async () => {
+	workbook.xlsx.readFile('./public/userdata.xlsx').then(async () => {
 		const worksheet = workbook.getWorksheet('Sheet1')
 		const db = client.db('lugedemo')
-		// worksheet.eachRow({ includeEmpty: false }, async (row, rowNum) => {
-		// 	const obj = {
-		// 		uid: row.values[1],
-		// 		name: row.values[2],
-		// 		avatar: row.values[3],
-		// 		is_signin: row.values[4],
-		// 		signin_time: row.values[5],
-		// 	}
-		// 	await db.collection('users').insert(obj)
-		// })
 		worksheet.eachRow({ includeEmpty: false }, async (row, rowNum) => {
 			const obj = {
 				uid: row.values[1],
-				content: row.values[2],
-				comment_time: Date.now()
+				name: row.values[2],
+				avatar: row.values[3],
+				is_signin: row.values[4],
+				signin_time: row.values[5],
 			}
-			await db.collection('comments').insert(obj)
+			await db.collection('users').insert(obj)
 			client.close()
 		})
+		// worksheet.eachRow({ includeEmpty: false }, async (row, rowNum) => {
+		// 	const obj = {
+		// 		uid: row.values[1],
+		// 		content: row.values[2],
+		// 		comment_time: Date.now()
+		// 	}
+		// 	await db.collection('comments').insert(obj)
+		// 	client.close()
+		// })
 	})
 })()
